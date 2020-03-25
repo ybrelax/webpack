22e4dc4a3d20f8cf149fe0821a7a9c90f5af3e26
yaobo <1195022367@qq.com>
2020-03-25 19:00:27 +0800

    新增进阶项目

--- first-basis/public/config.js
+++ first-basis/public/config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    dev: {
-        template: {
-            title: '你好',
-            header: false,
-            footer: false
-        }
-    },
-    build: {
-        template: {
-            title: '你好才怪',
-            header: true,
-            footer: false
-        }
+  dev: {
+    template: {
+      title: "这个是开发的",
+      header: false,
+      footer: false
     }
-}
+  },
+  build: {
+    template: {
+      title: "这个是创建的",
+      header: true,
+      footer: false
+    }
+  }
+};
---
+++ second-advance/webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require('path');
+const HtmlWbapckPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+
+  module: {
+    rules: [
+
+    ]
+  },
+  plugins: [
+    new HtmlWbapckPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'public/js/*.js',
+        to: path.resolve(__dirname, 'dist', 'js'),
+        flatten: true 
+      }
+    ],  {
+      ignore: ['other.js']
+    })
+  ]
+}
